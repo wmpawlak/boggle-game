b96d8b7a7176a0ac8c5976952df9f7e7637a5f94
Wojtek Pawlak <wojciech.pawlak92@gmail.com>
2019-10-23 23:21:53 +0200

    set up redux structure, babel, eslint, prettier, jest, enzyme, basics for circleci

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,56 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const letters = [
+    ['e', 'r', 'w', 't', 'a', 'ą'],
+    ['o', 't', 'o', 't', 'w', 'a'],
+    ['g', 'a', 'n', 'k', 'e', 'a'],
+    ['b', 'j', 'o', 'a', 'b', 'o'],
+    ['l', 'i', 'd', 'e', 'r', 'ś'],
+    ['g', 'b', 'n', 'h', 'w', 'e'], //from 6 first arrays, first letter is double points bonus
     ['ę', 'ż', 'n', 'z', 'l', 'r'],
     ['i', 't', 'ł', 's', 'y', 'd'],
-    ['e', 'r', 'w', 't', 'a', 'ą'],
     ['o', 's', 'e', 'i', 's', 't'],
     ['y', 'ć', 'r', 'e', 'l', 't'],
-    ['t', 'o', 'o', 't', 'w', 'a'],
     ['p', 'o', 'a', 'c', 's', 'h'],
-    ['a', 'n', 'k', 'e', 'g', 'a'],
     ['i', 'o', 'c', 'm', 't', 'u'],
     ['f', 'c', 'a', 'p', 's', 'k'],
-    ['j', 'o', 'a', 'b', 'b', 'o'],
     ['l', 'd', 'r', 'e', 'y', 'ź'],
     ['e', 's', 'u', 'e', 'i', 'k'],
-    ['ł', 'n', 'u', 'i', 'h', 'm'],
-    ['i', 'd', 'e', 'r', 'l', 'ś'],
-    ['b', 'g', 'n', 'h', 'w', 'e']
+    ['ł', 'n', 'u', 'i', 'h', 'm']
   ];
 
   const tab = [];
   for (let i = 0; i < letters.length; i++) {
     const drawnIndex = Math.floor(Math.random() * letters[i].length);
-    const letter = letters[i][drawnIndex]
+    const letter = letters[i][drawnIndex];
     tab.push(letter);
-  };
-
-  console.table(tab);
+  }
   const shuffledTab = tab;
   shuffledTab.sort(() => Math.random() - 0.5);
   console.table(shuffledTab);
 
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>tablica</p>
     </div>
   );
 }
---
+++ src/components/Letter/Letter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Letter = () => {
+  return (
+    <div className="letter">
+      <p>kjfhsadjkf</p>
+    </div>
+  )
+}
+
+export default Letter;
