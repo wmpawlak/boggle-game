d882d1ecc6451b6047d8287e5152024f795fd0db
Wojtek Pawlak <wojciech.pawlak92@gmail.com>
2019-10-24 22:47:12 +0200

    Letter component is showing specific letter

--- src/components/Letter/Letter.js
+++ src/components/Letter/Letter.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const Letter = i => {
-  console.log(i);
-  return (
-    <div className="letter">
-      ljfjdslhf
-    </div>
-  );
+const Letter = ({ index, lettersBoard }) => {
+  return <div className="letter">{lettersBoard[index]}</div>;
 };
 
-export default Letter;
+Letter.propTypes = {
+  index: PropTypes.number,
+  lettersBoard: PropTypes.array
+};
+
+const mapStateToProps = state => ({
+  lettersBoard: state.lettersBoard
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(Letter);
