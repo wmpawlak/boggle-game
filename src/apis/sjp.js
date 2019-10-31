aae468d33eca88418bc62a70d9c2c798172aa668
Wojtek Pawlak <wojciech.pawlak92@gmail.com>
2019-10-31 15:41:18 +0100

    checking word is working, problem with cors

--- src/apis/sjp.js
+++ src/apis/sjp.js
@@ -1,17 +1,17 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+// const axios = require('axios');
+// const cheerio = require('cheerio');
 
-const url = 'https://sjp.pl/';
+// const url = 'https://sjp.pl/';
 
-axios
-  .get(url)
-  .then(response => {
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const word = $('p')
-      .first()
-      .text();
+// axios
+//   .get(url)
+//   .then(response => {
+//     const html = response.data;
+//     const $ = cheerio.load(html);
+//     const word = $('p')
+//       .first()
+//       .text();
 
-    console.log(word);
-  })
-  .catch(console.error);
+//     console.log(word);
+//   })
+//   .catch(console.error);
