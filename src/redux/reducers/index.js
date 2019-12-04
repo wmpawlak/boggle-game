681b646eddde59c3d8ee3e674cf0b778f45fe098
Wojtek Pawlak <wojciech.pawlak92@gmail.com>
2019-12-04 13:51:43 +0100

    check

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -21,7 +21,7 @@ export const defaultState = {
     ['ł', 'n', 'u', 'i', 'h', 'm'],
   ],
   sessionLength: 3,
-  clockTime: 180,
+  clockTime: 5,
   secondsElapsed: 0,
   isTimerRunning: false,
   isTimerPaused: false,
