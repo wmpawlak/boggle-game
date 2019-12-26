2681e1ff1d69ca41d1d54936ad9421be615eefd0
Wojtek Pawlak <wojciech.pawlak92@gmail.com>
2019-12-26 22:09:36 +0100

    working on styles in rules modal

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -29,20 +29,4 @@ export const defaultState = {
   isAlarmON: true,
   isAutoStartON: true,
   isRulesModalOn: false,
-  rulesText: `
-Twoja ulubiona gra słowna teraz zawsze pod ręką!
-Możesz grać sam, ale prawdziwa zabawa zaczyna się gdy zaprosisz znajomych.
-Plansza składa się z 16 pól, na których znajdują się wylosowane litery.Poza grą potrzebujesz jedynie kartki i czegoś do pisania dla każdego z graczy.Zadaniem graczy jest znalezienie jak największej liczby słów w ciągu trzyminutowej rundy i zapisanie ich na kartkach.
-Słowa można tworzyć zgodnie z następującymi zasadami:
-Litery w słowie muszą tworzyć "łańcuch" czyli litery sąsiadujące ze sobą w słowie muszą sąsiadować ze sobą również na planszy(przylegać do siebie bokami lub rogami).Danego pola z literą nie można wykorzystać w stworzonym słowie więcej niż raz.Słowa muszą zawierać przynamniej trzy litery.
-Gra rozpoczyna się po wylosowaniu liter.W ciągu rundy należy odnaleźć jak najwięcej słów.O upływającym czasie informuje zegar znajdujący się nad planszą, a o końcu rundy powiadomi alarm.
-Po zakończeniu rundy gracze odczytują zapisane przez siebie słowa i przyznają punkty.Punkty można otrzymać za słowa, których nie ma żaden inny gracz oraz są dopuszczalne w grach według Słownika Języka Polskiego(co można sprawdzić w polu "sprawdź słowo").
-Punkty przyznawane są zgodnie z następującą punktacją:
-  - słowa 3- i 4- literowe: 1 punkt
-  - słowa 5-literowe: 2 punkty
-  - słowa 6-literowe: 3 punkty
-  - słowa 7-literowe: 4 punkty
-  - słowa 8- i więcej literowe: 11 punktów
-Po podliczeniu punktów można wylosować litery do kolejnej rundy i kontynuować zabawę!
-`
 };
